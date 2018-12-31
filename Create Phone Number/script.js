fb04f7466995b445511ff1ec4ee09f3f6b50da88
Nikola Todorovski <45461687+nTodorovski@users.noreply.github.com>
2018-12-31 16:47:09 +0100

    Add files via upload

---
+++ Create Phone Number/script.js
@@ -0,0 +1,4 @@
+function createPhoneNumber(numbers){
+    let newStr = `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`;
+    return newStr;
+}
